1825a0aa7f0abd15a422c36790d543e6e5b8b281
lining-lo <2092576148@qq.com>
2026-09-21 14:17:53 +0800

    feat:项目初始化

---
+++ qk-parent/qk-entity/src/main/java/com/qk/entity/User.java
@@ -0,0 +1,4 @@
+package com.qk.entity;
+
+public class User {
+}
---
+++ qk-parent/qk-management/src/test/java/com/qk/qkmanagement/QkManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qk.qkmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QkManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
